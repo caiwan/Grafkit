211d1197b88ea8bf5e7767ca08722e01d6646679
caiwan <caiwan.lupus@gmail.com>
2015-09-26 08:12:41 +0200

    matrix stub

---
+++ experiment/03/math/matrix.h
@@ -0,0 +1,27 @@
+#include "../render/dxtypes.h"
+
+class Matrix : public DirectX::XMMATRIX 
+{
+	// --- constructors
+	// overload if nesesarry
+
+	// === FACTORIES ===
+	// --- translation
+	static Matrix makeTranslate(float3 v) {
+		//Matrix mat;
+		//mat.
+	}
+
+	// --- scale
+
+	// --- rotate
+
+	// --- project
+
+	// --- ortho
+
+	// ===
+	// --- operations
+
+	// --- getters
+};
