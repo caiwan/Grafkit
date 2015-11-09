29f1ed8bf8dfa6137651d7da37bc54147e8d39ec
caiwan <caiwan.lupus@gmail.com>
2015-11-09 16:02:53 +0100

    vertex types + indices count added

--- src/api/render/mesh.h
+++ src/api/render/mesh.h
@@ -18,10 +18,18 @@ namespace FWrender
 	*/
 	class Mesh : public virtual Referencable{
 		public:
-			struct VertexType
+			struct VertexType_pos
+			{
+				float3 position;
+			};
+
+			// paddolni kell-e? 
+			struct VertexType_pos_tex
 			{
 				float3 position;
 				float2 texture;
+				float3 normal;
+				float3 tangent;
 			};
 
 		public:
@@ -30,24 +38,19 @@ namespace FWrender
 			virtual ~Mesh();
 
 			void Render(ID3D11DeviceContext* deviceContext);
-			void Shutdown();
+			
 
-			///@todo ez legyen kulon valasztva mindentol
-			// void CreateVertexBuffer(ID3D11Device* device, int vertexCount, const float3* position, const float2* uv);
-			// void CreateIndexBuffer(ID3D11Device* device, int indexCount, const int* indices);
-
-			void addElement(ID3D11Buffer * pBuffer) { this->m_buffers.push_back(pBuffer); }
-			void addIndices(ID3D11Buffer * pBuffer) { this->m_buffers.push_back(pBuffer); }
+			void addElement(ID3D11Buffer * pBuffer, int count) { this->m_buffers.push_back(pBuffer); this->m_vertexCount = count; }
+			void addIndices(ID3D11Buffer * pBuffer, int count) { this->m_indexBuffer = pBuffer; this->m_indexCount = count;}
 
 			int GetIndexCount() { return m_indexCount; }
 
 		protected:
 			void RenderBuffers(ID3D11DeviceContext* deviceContext);
 			void ShutdownBuffers();
-		
+			void Shutdown();
 
 		private:
-			// ID3D11Buffer *m_vertexBuffer, *m_indexBuffer;
 			ID3D11Buffer *m_indexBuffer;
 			std::vector<ID3D11Buffer *> m_buffers;
 			int m_vertexCount, m_indexCount;
