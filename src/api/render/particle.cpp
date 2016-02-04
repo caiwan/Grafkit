a2005345db3d27e4fec02e76a276ff7586fd0906
caiwan <caiwan.lupus@gmail.com>
2016-02-04 13:19:27 +0100

    particle system interface update - wil lremove entirely later

---
+++ src/api/render/particle.cpp
@@ -0,0 +1,9 @@
+#include "particle.h"
+
+FWrender::Particle::Particle(size_t n)
+{
+}
+
+FWrender::Particle::~Particle()
+{
+}
--- src/api/render/particle.h
+++ src/api/render/particle.h
@@ -1,40 +1,34 @@
 #include "renderer.h"
+#include "mesh.h"
 
+#define DEFAULT_PARTICLE_COUNT 4096
 
 namespace FWrender
 {
 	/** Particle generator
 	*/
 	class Particle 
-	{
-		protected:
-			struct particle{
-				float3 vertex;
-				UINT index;
-				float3 position;
-				float2 size;
-				float4 color;
-				float2 uv;
-				float angle;
-			};
-
-			/*typedef union {
-				struct {
-					bool has_vertex : 1;
-					bool has_index : 1;
-					bool has_position : 1;
-					bool has_size : 1;
-					bool has_color : 1;
-					bool has_uv : 1;
-					bool has_angle : 1;
-				};
-				UINT mask;
-			} cbuffer_flags;*/
-			
+	{	
 		public:
-			Particle();
+			Particle(size_t n = DEFAULT_PARTICLE_COUNT);
 			~Particle();
 
 			// input layout ? 
+
+		protected:
+			struct particle {
+				float3 vertex;
+				UINT index;
+				float2 uv;
+				
+				// a jovoben valami gpu accelerated loszar kell majd ehelyett
+				float3 position;
+				float2 size;
+				float4 color;
+				float angle;
+			};
+
+			struct particle m_pParticles;
+			size_t count;
 	};
 }
