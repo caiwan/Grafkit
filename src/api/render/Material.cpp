ab02c25671c24237a71b05fb1795363c48f5a1f1
caiwan <caiwan.lupus@gmail.com>
2016-02-13 21:30:35 +0100

    texures added - not so good, but okay

--- src/api/render/Material.cpp
+++ src/api/render/Material.cpp
@@ -1,3 +1,114 @@
 #include "Material.h"
 
-// it's empty for now.
+#include "shader.h"
+#include "texture.h"
+
+#include "easyloggingpp.h"
+
+using FWrender::Renderer;
+using FWrender::Texture;
+using FWrender::Shader;
+using FWrender::TextureRef;
+using FWrender::ShaderRef;
+using std::vector;
+
+FWrender::MaterialBase::MaterialBase()
+{
+	for (size_t i = 0; i < TT_COUNT; i++) {
+		this->m_texture_buckets[i] = vector<TextureRef>();
+	}
+}
+
+FWrender::MaterialBase::~MaterialBase()
+{
+	for (size_t i = 0; i < TT_COUNT; i++) {
+		this->m_texture_buckets[i].clear();
+	}
+}
+
+/** Texture names LUT*/
+namespace {
+	const char *map_names [] = 
+	{
+		"diffuse",
+
+		"alpha",
+		"normal",
+		"shiniess",
+		"specular",
+		"selfillum",
+		"reflect",
+		"bump",
+		/*
+		"shadowDepthtMap",
+		"sceneDephtMap",
+		"sceneTexture",
+		"glowFilter",
+		*/
+		"aux",
+	};
+}
+
+void FWrender::MaterialBase::Render(ID3D11DeviceContext * deviceContext)
+{
+	// setup cbuffers
+
+	// setup texutes 
+	for (size_t i = 0; i < this->m_reflected_textures.size(); i++) {
+		this->m_reflected_textures[i].brecord->SetTexture(this->m_reflected_textures[i].texture);
+	}
+}
+
+TextureRef & FWrender::MaterialBase::GetTexture(texture_type_e bucket, int n)
+{
+}
+
+void FWrender::MaterialBase::SetTexture(TextureRef texture, int n)
+{
+}
+
+void FWrender::MaterialBase::ReflectShader()
+{
+	// reflect cbuffer locations 
+	ReflectTextures();
+}
+
+void FWrender::MaterialBase::ReflectTextures()
+{
+	if (this->m_framgentShader.Invalid) {
+		LOG(TRACE) << "No valid shader was set, abort texture reflection.";
+		return;
+	}
+
+	char txname[256];
+
+	m_reflected_textures.clear();
+
+	for (size_t i = 0; i < TT_COUNT; i++) 
+	{
+		if (!this->m_texture_buckets[i].empty())
+		{
+			for (size_t j = 0; j < m_texture_buckets[i].size(); j++)
+			{
+				// search for names
+				sprintf_s(txname, "%s%d", map_names[i], j);
+				Shader::BoundResourceRecord *brecord = &(this->m_framgentShader->GetBResource(txname));
+				if (brecord->IsValid()) {
+					struct reflection_texture_entity entity;
+
+					// remeljuk nem akad ossze az elozoleg besetelt textura, ami nincs ebben benne
+					entity.brecord = brecord;
+					entity.texture = m_texture_buckets[i][j];
+					m_reflected_textures.push_back(entity);
+
+					LOG(TRACE) << "Reflected texture" << txname;
+				}
+				else {
+					LOG(TRACE) << "No reflection point found" << txname;
+				}
+
+			}
+		}
+	}
+
+}
--- src/api/render/Material.h
+++ src/api/render/Material.h
@@ -1,7 +1,12 @@
 #pragma once
 
+#include <vector>
+
 #include "dxtypes.h"
 #include "reference.h"
+#include "renderer.h"
+#include "shader.h"
+
 #include "../core/renderassets.h"
 
 namespace FWrender {
@@ -13,14 +18,43 @@ namespace FWrender {
 		float shininess;
 	};
 
-	class MaterialWrapper : virtual public Referencable, virtual public FWassets::IRenderAsset
+	///Based on 3ds format's 
+	enum texture_type_e {
+		// regular types	
+		TT_diffuse,	///< 1st map
+		
+		TT_alpha,		///< alpha map
+		TT_normal,		///< bump map
+		TT_shiniess,   ///< shininess map
+		TT_specular,   ///< specular map
+		TT_selfillum,	///< self illumination map
+		TT_reflect,	///< reflection map
+		TT_bump,		///< bump map
+
+		// ezek valoszinuleg post-effektezeshez kellenenek, ami nem itt lesz szamontartva lehetoleg
+		/*
+		TMT_shadowDepthtMap, ///< for depth map shadow
+		TMT_sceneDephtMap,	///< depth map of the scene
+		TMT_sceneTexture,	///< the scne rendered into a texture
+		TMT_glowFilter,	    ///< input of the glow filter
+		*/
+
+		TT_aux, ///< aux texture, used for pretty much everything else
+
+		TT_COUNT	// count
+	};
+
+	///@todo three.js mintajara tobbfele materrial tipust lehessen legyartani, ha kell~ 
+	class MaterialBase : virtual public Referencable, virtual public FWassets::IRenderAsset
 	{
 	public:
-		MaterialWrapper() {}
-		~MaterialWrapper() {}
+		MaterialBase();
+		~MaterialBase();
 
+		/// @todo ez nem ilyen lesz a jovoben
 		operator Material& () { return this->m_mater; }
 
+		/// @todo valahogy ide valamilyen kulcs-ertek parok formajaban kellene a cbuffer cuccait betolni. 
 		float4 &GetAmbient() { return m_mater.ambient; }
 		float4 &GetDiffuse() { return m_mater.diffuse; }
 		float4 &GetSpecular() { return m_mater.specular; }
@@ -28,10 +62,37 @@ namespace FWrender {
 		float &GetSpecularLevel() { return m_mater.specularLevel; }
 		float &GetShininess() { return m_mater.shininess; }
 
+		ShaderRef GetShader() { return this->m_framgentShader; }
+		void SetShader(ShaderRef shader) { this->m_framgentShader = shader; ReflectShader(); }
+
+		/// @todo slotok kezelese 
+		/// @todo bounds check
+		TextureRef &GetTexture(texture_type_e bucket, int n = 0);
+		void SetTexture(TextureRef texture, int n = 0);
+
+		size_t GetTextureBucketCount(texture_type_e bucket) { return this->m_texture_buckets[bucket].size(); }
+
+		virtual void Render(ID3D11DeviceContext* deviceContext);
+
 	protected:
+		/// Setup texture and cbufer locations
+		virtual void ReflectShader();
+		void ReflectTextures();
+
 		struct Material m_mater;
+
+		std::vector<TextureRef> m_texture_buckets[TT_COUNT];
+		ShaderRef m_framgentShader;
+
+		struct reflection_texture_entity 
+		{
+			FWrender::Shader::BoundResourceRecord *brecord;
+			TextureRef texture;
+		};
+
+		std::vector<struct reflection_texture_entity> m_reflected_textures;
 	};
 
-	typedef Ref<MaterialWrapper> MaterialRef;
+	typedef Ref<MaterialBase> MaterialRef;
 
 }
