b1e33829a255d8ad79c0d75226c3e4c5329babdc
caiwan <caiwan.lupus@gmail.com>
2016-02-18 16:00:59 +0100

    logging + resource filter + dirlist added

--- src/api/core/assets.cpp
+++ src/api/core/assets.cpp
@@ -1,20 +1,96 @@
-#include <stdio.h>
-
 #include "assets.h"
+
+#include <string.h>
+#include <stdio.h>
+#include "easyloggingpp.h"
+
 
 using namespace FWassets;
 using namespace FWdebugExceptions;
 
+// ====================================================================================
+
+FWassets::ResourceFilterExtension::ResourceFilterExtension(const char ** const & extensions, size_t count)
+	: IResourceFilter()
+{
+	for (size_t i = 0; i < count; i++) {
+		this->m_filterExtensions.insert(extensions[i]);
+	}
+}
+
+int FWassets::ResourceFilterExtension::isFileInfilter(std::string path)
+{	
+	// miezittkerem
+	char * in_str = new char[path.length()+1];
+	strcpy_s(in_str, path.length(), path.c_str());
+	rsize_t strmax = path.length();
+	char * pch = nullptr, *pchs[256], *next = nullptr;
+	const char *delim = ".";
+
+	size_t count = 0;
+	do 
+	{
+		// ez buzi ez teljesen 
+		pch = strtok_s (in_str, delim, &next);
+		pchs[count] = pch;
+	} while (pch != NULL);
+
+	if (count == 0) 
+		return 0;
+
+	char *& fext = pchs[count - 1];
+
+	int found = 0;
+	for (extFilter_t::iterator it = m_filterExtensions.begin(); it != m_filterExtensions.end(); it++) {
+		if (*it == fext) {
+			found = 1;
+			break;
+		}
+	}
+
+	delete in_str;
+
+	return found;
+}
+
 // implementation of file managers
 
-FWassets::FileResourceManager::FileResourceManager(const char * root) :
-	m_root(root)
-{
+#include "dirent.h"
+
+typedef FWassets::FileResourceManager::fileleist_t _filelist_t;
+
+_filelist_t listdir(std::string root, _filelist_t &dirlist) {
+	DIR *dir;
+	struct dirent *ent;
+	if ((dir = opendir(root.c_str())) != NULL) {
+		while ((ent = readdir(dir)) != NULL) {
+			// skip root and parent
+			if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0) 
+				continue;
+
+			if (ent->d_type & DT_DIR) {
+				listdir(root + ent->d_name +"/", dirlist);
+			}
+			else {
+				dirlist.push_back(root + std::string(ent->d_name));
+			}
+		}
+		closedir(dir);
+	}
+	else {
+		/* could not open directory */
+		// return EXIT_FAILURE;
+
+		///@todo raise exception
+	}
+
+	return dirlist;
 }
 
 FWassets::FileResourceManager::FileResourceManager(std::string root) : 
 	m_root(root)
 {
+	listdir(root, m_dirlist);
 }
 
 FWassets::FileResourceManager::~FileResourceManager()
@@ -29,6 +105,8 @@ IResourceRef FWassets::FileResourceManager::GetResourceByName(std::string name)
 
 	std::string fullname = m_root + name;
 	fopen_s(&fp, fullname.c_str(), "rb");
+
+	LOG(TRACE) << "Accessing file " << fullname.c_str();
 
 	if (!fp) {
 		throw EX_DETAILS(AssetLoadException, fullname.c_str());
@@ -46,8 +124,24 @@ IResourceRef FWassets::FileResourceManager::GetResourceByName(std::string name)
 
 IResourceRef FWassets::FileResourceManager::GetResourceByUUID(Guid uuid)
 {
-	throw EX_DETAILS(NotImplementedMethodException, "Loading file from uuid is not implemented. Not even close.");
+	// throw EX_DETAILS(NotImplementedMethodException, "Loading file from uuid is not implemented. Not even close.");
 	return IResourceRef();
+}
+
+std::list<std::string> FWassets::FileResourceManager::GetResourceList()
+{
+	return this->m_dirlist;
+}
+
+std::list<std::string> FWassets::FileResourceManager::GetResourceList(IResourceFilter * filter)
+{
+	_filelist_t filelist;
+	for (fileleist_t::iterator it = m_dirlist.begin(); it != m_dirlist.end(); it++)
+	{
+		if (filter->isFileInfilter(*it)) filelist.push_back(*it);
+	}
+
+	return filelist;
 }
 
 // ==================================================================================== 
--- src/api/core/assets.h
+++ src/api/core/assets.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <string>
+#include <list>
+#include <set>
+#include <string>
 
 #include "exceptions.h"
 
@@ -23,16 +26,51 @@ namespace FWassets {
 		virtual size_t GetSize() = 0;
 	};
 
+	// ----------
+	/// @todo ezt kulon fileba majd, ha lehet, vagy nem 
+	/// An interface to create filters over resources
+	class IResourceFilter
+	{
+	public:
+		IResourceFilter() {}
+		virtual ~IResourceFilter() {}
+
+		virtual int isFileInfilter(std::string path) = 0;
+
+	};
+
+	class ResourceFilterExtension : IResourceFilter
+	{
+	public:
+		ResourceFilterExtension(const char ** const & extensions, size_t count);
+		virtual ~ResourceFilterExtension() {}
+
+		int isFileInfilter(std::string path);
+
+	private:
+		typedef std::set<std::string> extFilter_t;
+		extFilter_t m_filterExtensions;
+
+	};
+
+	// ----------
+
 	/// An interface for resource obtaining mechnaism
 	class IResourceFactory 
 	{
 	public:
 		IResourceFactory() {}
+		// IResourceFactory(const char ** const & extensions, size_t count);
+		
 		virtual ~IResourceFactory() {}
 
 	public:
 		virtual IResourceRef GetResourceByName(std::string name) = 0;
 		virtual IResourceRef GetResourceByUUID(Guid uuid) = 0;
+
+		typedef std::list<std::string> fileleist_t;
+		virtual fileleist_t GetResourceList() { return std::list<std::string>(); };
+
 
 	};
 
@@ -45,14 +83,19 @@ namespace FWassets {
 		class FileResource;
 
 	public:
-		FileResourceManager(const char* root);
+		//FileResourceManager(const char* root);
 		FileResourceManager(std::string root);
 		~FileResourceManager();
 
 		virtual IResourceRef GetResourceByName(std::string name);
 		virtual IResourceRef GetResourceByUUID(Guid uuid);
+
+		virtual std::list<std::string> GetResourceList();
+		virtual std::list<std::string> GetResourceList(IResourceFilter * filter);
+
 	private:
 		std::string m_root;
+		fileleist_t m_dirlist;
 
 	public:
 		class FileResource : public IResource {
