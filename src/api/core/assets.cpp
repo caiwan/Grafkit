ef22ec79d62ea281c4103aba8b6ac6b5ed6b12d6
caiwan <caiwan.lupus@gmail.com>
2016-02-21 19:10:48 +0100

    trimpath full

--- src/api/core/assets.cpp
+++ src/api/core/assets.cpp
@@ -21,20 +21,31 @@ FWassets::ResourceFilter::ResourceFilter(const char ** const & extensions, size_
 
 int FWassets::ResourceFilter::trimpath(std::string fullpath, std::string &path, std::string &filename, std::string & extension)
 {
-	int result = 0; //, rcc = 0;
+	int result = 0;
 
-	size_t in_len = fullpath.length();
+	size_t in_len = fullpath.length(), n = 0;
 	const char * in_str = fullpath.c_str();
-	const char delim = '.';
 
-	size_t n = in_len + 1;
-	while (n--) if (in_str[n] == delim) break;
+	// finding extension
+	n = in_len + 1;
+	while (n--) if (in_str[n] == '.') break;
 
 	const char * fext = nullptr;
 	if (!n)  result |= 1 << 0; else  fext = &in_str[n + 1];
 	if (fext) extension = fext; else result |= 1 << 1;
 
-	/// @todo path levalasztasa goez here
+	// finding filename
+	// reset or continue 
+	if (!n) n = in_len + 1; else n++;
+	while (n--) if (in_str[n] == '/') break;
+
+	// setup name
+	const char * fname = nullptr;
+	if (!n)  result |= 1 << 2; else  fname = &in_str[n + 1];
+	if (fext) filename = fname; else result |= 1 << 3;
+
+	// trim out path
+	path.assign(fullpath, 0, n+1);
 
 	return result;
 }
