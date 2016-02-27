51f48da03a1e389cf581a927494a592dff0125df
caiwan <caiwan.lupus@gmail.com>
2016-02-27 15:29:00 +0100

    Scenegraph kicsupalva

--- src/api/render/Scenegraph.cpp
+++ src/api/render/Scenegraph.cpp
@@ -7,38 +7,30 @@ using FWrender::Entity3D;
 FWrender::Scenegraph::Scenegraph():
 	m_pRoot(nullptr)
 {
-	m_pRoot = new ListTree();
-	m_pRoot->setObject(this);
 }
+
 
 FWrender::Scenegraph::~Scenegraph()
 {
 	delete m_pRoot;
 }
 
+
 void FWrender::Scenegraph::Render(FWrender::Renderer &render)
 {
-	m_render = &render;
-	m_pRoot->parse(this);
 }
 
-void FWrender::Scenegraph::parseNode(TreeNode * node)
+
+void FWrender::Scenegraph::renderNode(FWrender::Renderer & render, Actor *& p_actor)
 {
-	if (!(node && node->getObject()))
-		return;
-	
-	///@todo ezzel a szarral kell kezdeni meg valamit
-	//ActorRef actor = (Actor*)node->getObject();
-	Actor* actor = reinterpret_cast<Actor*>(node->getObject());
-	actor->Render(m_render);
-
-	actor->Render(*(m_render));
 }
+
 
 void FWrender::Scenegraph::push()
 {
 	m_matrix_stack.push(m_matrix);
 }
+
 
 void FWrender::Scenegraph::pop()
 {
--- src/api/render/Scenegraph.h
+++ src/api/render/Scenegraph.h
@@ -1,30 +1,30 @@
 #pragma once
 #include <stack>
 #include "../math/matrix.h"
-#include "tree.h"
+#include "Actor.h"
+//#include "tree.h"
 #include "renderer.h"
 
 namespace FWrender {
-	class Scenegraph : virtual public Referencable, public TreeParser
+	class Scenegraph : virtual public Referencable //, public TreeParser
 	{
 		public:
 			Scenegraph();
 			~Scenegraph();
 
-			///@todo implement
 			void Render(FWrender::Renderer & render);
 
 		// -- tree parser implementation
 		protected:
-			virtual void parseNode(TreeNode* node);
-			virtual void push();
-			virtual void pop();
+			void renderNode(FWrender::Renderer & render, Actor *& p_actor);
+			void push();
+			void pop();
 
 		private:
 			FWmath::Matrix m_matrix;
 			std::stack<FWmath::Matrix> m_matrix_stack;
 
-			ListTree *m_pRoot;
-			FWrender::Renderer * m_render; ///@todo ezzel itt kezdeni kell valamit
+			Actor* m_pRoot;
+
 	};
 }
