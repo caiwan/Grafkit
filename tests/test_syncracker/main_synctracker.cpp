7ca222ec03a7eb04d80a86211d8668858303fbb5
Sári Péter <Sri Pter>
2016-06-01 15:42:07 +0200

    synctracker sample ok

--- tests/test_syncracker/main_synctracker.cpp
+++ tests/test_syncracker/main_synctracker.cpp
@@ -54,9 +54,26 @@ protected:
 	Timer* timer;
 	ValueTracker *valTracker;
 
+	ValueTracker::Float3Track *trk_rotate_root;
+	ValueTracker::Float2Track *trk_rotate_r;
+	ValueTracker::Float2Track *trk_rotate_l;
+	ValueTracker::Float2Track *trk_rotate_u;
+	ValueTracker::Float2Track *trk_rotate_d;
+	ValueTracker::Float2Track *trk_rotate_f;
+	ValueTracker::Float2Track *trk_rotate_b;
+
 	TextureSamplerRef m_textureSampler;
 
 	ActorRef m_rootActor;
+
+	ActorRef cameraActor;
+	ActorRef modelActor	;
+	ActorRef modelActorL;
+	ActorRef modelActorR;
+	ActorRef modelActorU;
+	ActorRef modelActorD;
+	ActorRef modelActorF;
+	ActorRef modelActorB;
 
 	float t;
 
@@ -112,17 +129,17 @@ protected:
 
 		// -- setup scene 
 		scene = new Scene();
-		ActorRef cameraActor = new Actor(); cameraActor->AddEntity(camera);
-		ActorRef modelActor = new Actor(); modelActor->AddEntity(model);
-
-		ActorRef modelActorL = new Actor(); modelActorL->AddEntity(model); modelActorL->Matrix().Translate(3, 0, 0); modelActor->AddChild(modelActorL);
-		ActorRef modelActorR = new Actor(); modelActorR->AddEntity(model); modelActorR->Matrix().Translate(-3, 0, 0); modelActor->AddChild(modelActorR);
-
-		ActorRef modelActorU = new Actor(); modelActorU->AddEntity(model);  modelActorU->Matrix().Translate(0, 3, 0); modelActor->AddChild(modelActorU);
-		ActorRef modelActorD = new Actor(); modelActorD->AddEntity(model);  modelActorD->Matrix().Translate(0, -3, 0); modelActor->AddChild(modelActorD);
-
-		ActorRef modelActorF = new Actor(); modelActorF->AddEntity(model); modelActorF->Matrix().Translate(0, 0, 3); modelActor->AddChild(modelActorF);
-		ActorRef modelActorB = new Actor(); modelActorB->AddEntity(model); modelActorB->Matrix().Translate(0, 0, -3); modelActor->AddChild(modelActorB);
+		this->cameraActor = new Actor(); cameraActor->AddEntity(camera);
+		this->modelActor = new Actor(); modelActor->AddEntity(model);
+		
+		this->modelActorL = new Actor(); modelActorL->AddEntity(model); modelActorL->Matrix().Translate(3, 0, 0); modelActor->AddChild(modelActorL);
+		this->modelActorR = new Actor(); modelActorR->AddEntity(model); modelActorR->Matrix().Translate(-3, 0, 0); modelActor->AddChild(modelActorR);
+		
+		this->modelActorU = new Actor(); modelActorU->AddEntity(model);  modelActorU->Matrix().Translate(0, 3, 0); modelActor->AddChild(modelActorU);
+		this->modelActorD = new Actor(); modelActorD->AddEntity(model);  modelActorD->Matrix().Translate(0, -3, 0); modelActor->AddChild(modelActorD);
+		
+		this->modelActorF = new Actor(); modelActorF->AddEntity(model); modelActorF->Matrix().Translate(0, 0, 3); modelActor->AddChild(modelActorF);
+		this->modelActorB = new Actor(); modelActorB->AddEntity(model); modelActorB->Matrix().Translate(0, 0, -3); modelActor->AddChild(modelActorB);
 
 
 		// ActorRef lightActor = new Actor(); lightActor->AddEntity(light);
@@ -148,6 +165,16 @@ protected:
 		timer->Connect();
 		valTracker = new ValueTracker(timer);
 
+		
+		this->trk_rotate_root = valTracker->newFloat3Track("root", "r", "rpy");
+		this->trk_rotate_r = valTracker->newFloat2Track("r", "r", "rpy");
+		this->trk_rotate_l = valTracker->newFloat2Track("l", "r", "rpy");
+		this->trk_rotate_u = valTracker->newFloat2Track("u", "r", "rpy");
+		this->trk_rotate_d = valTracker->newFloat2Track("d", "r", "rpy");
+		this->trk_rotate_f = valTracker->newFloat2Track("f", "r", "rpy");
+		this->trk_rotate_b = valTracker->newFloat2Track("b", "r", "rpy");
+		
+
 		this->t = 0;
 
 		return 0;
@@ -169,8 +196,18 @@ protected:
 
 			ShaderRef fragmentShader = this->m_fragmentShader->Get();
 
-			m_rootActor->Matrix().Identity();
-			m_rootActor->Matrix().RotateRPY(t, 0, 0);
+			float2 rot_rp;
+			float3 rot_rpy;
+			
+			rot_rpy = trk_rotate_root->Get(); m_rootActor->Matrix().Identity(); m_rootActor->Matrix().RotateRPY(rot_rpy.x, rot_rpy.y, rot_rpy.z);
+
+			rot_rp = trk_rotate_r->Get(); modelActorR->Transform().Identity(); modelActorR->Transform().RotateRPY(rot_rp.x, rot_rp.y,0);
+			rot_rp = trk_rotate_l->Get(); modelActorL->Transform().Identity(); modelActorL->Transform().RotateRPY(rot_rp.x, rot_rp.y,0);
+			rot_rp = trk_rotate_u->Get(); modelActorU->Transform().Identity(); modelActorU->Transform().RotateRPY(rot_rp.x, rot_rp.y,0);
+			rot_rp = trk_rotate_d->Get(); modelActorD->Transform().Identity(); modelActorD->Transform().RotateRPY(rot_rp.x, rot_rp.y,0);
+			rot_rp = trk_rotate_f->Get(); modelActorF->Transform().Identity(); modelActorF->Transform().RotateRPY(rot_rp.x, rot_rp.y,0);
+			rot_rp = trk_rotate_b->Get(); modelActorB->Transform().Identity(); modelActorB->Transform().RotateRPY(rot_rp.x, rot_rp.y,0);
+			
 
 			fragmentShader->GetBRes("SampleType").Set(m_textureSampler->GetSamplerState());
 
