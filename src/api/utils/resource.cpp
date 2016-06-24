475acaeec96a0f203472befa243056c03d196a5d
caiwan <caiwan.lupus@gmail.com>
2016-06-24 09:31:23 +0200

    old guid rm'd

--- src/api/utils/resource.cpp
+++ src/api/utils/resource.cpp
@@ -3,35 +3,20 @@
 
 using namespace Grafkit;
 
-
 IResource::IResource() 
 {
-#ifndef _NO_UUID_	
-	GuidGenerator gen;
-	this->m_guid = gen.newGuid();
-#endif 
 }
 
 IResource::~IResource()
 {
-	///@todo a destruktora ennek nem fasza, a virtualis fuggvenyeket a leszarmazott deletekor viszi magaval. Valami koser megoldas kellene a torlesre, ami megkeruli ezt 
+	/**
+	@todo a destruktora ennek nem fasza, a virtualis fuggvenyeket a leszarmazott deletekor viszi magaval. 
+	Valami koser megoldas kellene a torlesre, ami megkeruli ezt 
+	*/
 }
 
-void IResource::SetName(std::string name)
+void IResource::SetName(std::string name) : m_name(name)
 {
-	m_name = name;
 }
 
-#ifndef _NO_UUID_
-Guid FWassets::IRenderAsset::GenerateUUID()
-{
-	Guid uuid = m_guid;
-	
-	GuidGenerator gen;
-	uuid = gen.newGuid();
 
-	m_guid = uuid;
-
-	return this->m_guid;
-}
-#endif
--- src/api/utils/resource.h
+++ src/api/utils/resource.h
@@ -7,14 +7,6 @@ A generator interface for assets
 #include <map>
 #include <string>
 
-//#include "memory_align.h"
-
-// egyelore nem szerencsetlenkedunk a guid-dal, de ha megirtam eddig, akkor maradjon
-#define _NO_UUID_
-
-#ifndef _NO_UUID_
-#include "guid.h"
-#endif 
 
 #include "reference.h"
 #include "exceptions.h"
@@ -40,17 +32,6 @@ namespace Grafkit{
 
 		std::string GetName() { return this->m_name; }
 		void SetName(std::string name);
-
-#ifndef _NO_UUID_
-		Guid GetUUID() { return this->m_guid; }
-		Guid GenerateUUID();
-		void SetUUID(Guid uuid) { this->m_guid = uuid; }
-	protected:
-		Guid m_guid;
-#endif // _NO_UUID_
-
-		// erre valami mas megoldst kellene talalni majd 
-		// virtual const char* GetBucketID() = 0;
 
 	protected:
 		/// Sets asset manager 
