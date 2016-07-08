b72a40332d4a1b33ff38b1159e7c4004b1a5afd6
caiwan <caiwan.lupus@gmail.com>
2016-07-08 19:12:32 +0200

    hlsl update

--- tests/assets/shaders/default.hlsl
+++ tests/assets/shaders/default.hlsl
@@ -1,8 +1,142 @@
-#include "commons/projection.hlsl"
-#include "commons/lightmaterial.hlsl"
+cbuffer MatrixBuffer
+{
+	matrix worldMatrix;
+	matrix viewMatrix;
+	matrix projectionMatrix;
+};
+
+/**
+* http://www.rastertek.com/dx10tut06.html
+* http://www.rastertek.com/dx10tut10.html
+* http://www.rastertek.com/dx11tut30.html
+*
+*/
+
+cbuffer material
+{
+	struct {
+		int m_type;
+		float4 m_ambient, m_diffuse, m_specular, m_emission;
+		float m_specularLevel;
+		float m_shininess;
+	} material;
+};
+
+cbuffer light
+{
+	int is_lightOn[4];
+
+	struct
+	{
+		int l_type;
+		float4 l_position;
+		float4 l_direction;
+
+		float4 l_ambient;
+		float4 l_diffuse;
+		float4 l_specular;
+
+		float l_ca, l_la, l_qa;
+
+		float l_angle, l_falloff;
+	} lights[4];
+
+}
+
+struct VertexInputType
+{
+	float4 position : POSITION;
+	float4 normal :	NORMAL;
+	float4 tangent : TANGENT;
+	float4 binormal : BINORMAL;
+	float2 tex : TEXCOORD;
+};
+
+struct PixelInputType
+{
+	float4 position : SV_POSITION;
+	float4 normal : NORMAL;
+	float4 tangent : TANGENT;
+	float4 binormal : BINORMAL;
+	
+	float2 tex : TEXCOORD0;
+
+	float4 lightPos1 : TEXCOORD1;
+	float4 lightPos2 : TEXCOORD2;
+	float4 lightPos3 : TEXCOORD3;
+	float4 lightPos4 : TEXCOORD4;
+};
+
+
+SamplerState SampleType {
+	Filter = MIN_MAG_MIP_LINEAR;
+	AddressU = Wrap;
+	AddressV = Wrap;
+};;
+
+Texture2D t_diffuse;
+Texture2D t_normal;
+Texture2D t_specular;
+
 
 PixelInputType mainVertex(VertexInputType input)
 {
-	PixelInputType output = projectScene(input);
+	PixelInputType output;
+
+	input.position.w = 1.0f;
+	output.position = input.position;
+	output.position = mul(output.position, worldMatrix);
+	output.position = mul(output.position, viewMatrix);
+	output.position = mul(output.position, projectionMatrix);
+
+	input.normal.w = 0.0f;
+	output.normal = input.normal;
+	output.normal = mul(output.normal, worldMatrix);
+	output.normal = mul(output.normal, viewMatrix);
+	output.normal = mul(output.normal, projectionMatrix);
+	output.normal = normalize(output.normal);
+
+	input.tangent.w = 0.0f;
+	output.tangent = input.tangent;
+	output.tangent = mul(output.tangent, worldMatrix);
+	output.tangent = mul(output.tangent, viewMatrix);
+	output.tangent = mul(output.tangent, projectionMatrix);
+	output.tangent = normalize(output.tangent);
+
+	input.binormal.w = 0.0f;
+	output.binormal = input.binormal;
+	output.binormal = mul(output.binormal, worldMatrix);
+	output.binormal = mul(output.binormal, viewMatrix);
+	output.binormal = mul(output.binormal, projectionMatrix);
+	output.binormal = normalize(output.binormal);
+
+	//output.lightPos1.w = 0.0f;
+	//output.lightPos2.w = 0.0f;
+	//output.lightPos3.w = 0.0f;
+	//output.lightPos4.w = 0.0f;
+
+	//output.lightPos1.xyz = lights[0].l_position.xyz - output.position.xyz;
+	//output.lightPos2.xyz = lights[1].l_position.xyz - output.position.xyz;
+	//output.lightPos3.xyz = lights[2].l_position.xyz - output.position.xyz;
+	//output.lightPos4.xyz = lights[3].l_position.xyz - output.position.xyz;
+
+	//output.lightPos1 = normalize(output.lightPos1);
+	//output.lightPos2 = normalize(output.lightPos2);
+	//output.lightPos3 = normalize(output.lightPos3);
+	//output.lightPos4 = normalize(output.lightPos4);
+
+	output.tex = input.tex;
+
 	return output;
+}
+
+// PixelShader
+//------------------------------------------------------------------------------------
+float4 mainPixel(PixelInputType input) : SV_TARGET
+{
+	float4 textureColor = float4(0,0,0,1);
+	textureColor = t_diffuse.Sample(SampleType, input.tex);
+	//textureColor.x = 0;
+	//textureColor.w = 1;
+	return textureColor;
 }
--- tests/assets/shaders/texture.hlsl
+++ tests/assets/shaders/texture.hlsl
@@ -1,20 +1,2 @@
-#include "commons/projection.hlsl"
+#include "projection.hlsl"
 
-// GLOBALS //
-
-
-
-Texture2D diffuse;
-SamplerState SampleType;
-
-
-// PixelShader
-//------------------------------------------------------------------------------------
-float4 mainPixel(PixelInputType input) : SV_TARGET
-{
-	float4 textureColor = float4(0,0,0,1);
-	textureColor = diffuse.Sample(SampleType, input.tex);
-	//textureColor.x = 0;
-	//textureColor.w = 1;
-	return textureColor;
-}
