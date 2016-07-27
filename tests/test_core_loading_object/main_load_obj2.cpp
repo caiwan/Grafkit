5003d5ad39b1496f1a10fc0fc02c534d00b311e8
caiwan <caiwan.lupus@gmail.com>
2016-07-27 10:17:56 +0200

    test update, loads assbin + uses phong shading

--- tests/test_core_loading_object/main_load_obj2.cpp
+++ tests/test_core_loading_object/main_load_obj2.cpp
@@ -69,10 +69,10 @@ protected:
 
 		// -- load shader
 		m_vertexShader = Load<ShaderRes>(new ShaderLoader("vShader", "shaders/default.hlsl", "mainVertex", ST_Vertex));
-		m_fragmentShader = Load<ShaderRes>(new ShaderLoader("pShader", "shaders/default.hlsl", "mainPixel", ST_Pixel));
+		m_fragmentShader = Load<ShaderRes>(new ShaderLoader("pShader", "shaders/default.hlsl", "mainPixelPhongNoTexture", ST_Pixel));
 
 		// -- model 
-		m_scene = this->Load<SceneRes>(new AssimpLoader("models/shphere.assbin", m_vertexShader));
+		m_scene = this->Load<SceneRes>(new AssimpLoader("models/sphere.assbin", m_vertexShader));
 
 		m_t = 0;
 
