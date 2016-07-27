375ae86fa306c7214041fdefe2b612955c7162e3
caiwan <caiwan.lupus@gmail.com>
2016-07-27 18:01:03 +0200

    camera fuckup

--- src/api/render/camera.cpp
+++ src/api/render/camera.cpp
@@ -64,35 +64,57 @@ void Camera::SetRotation(float x, float y, float z)
 // --- pinakobold
 void Camera::Calculate(Renderer& renderer)
 {
+
+	// lofasz a seggedbe kis geci 
+
+	float3 pos = float3(0, 0, 5);
+	float3 center = float3(0, 0, 0);
+	float3 up = float3(0,1,0);
+ 
+	XMVECTOR v_pos = XMLoadFloat3(&pos);
+	XMVECTOR v_center = XMLoadFloat3(&center);
+	XMVECTOR v_up = XMLoadFloat3(&up);
+
+	m_viewMatrix = XMMatrixLookAtLH(v_pos, v_center, v_up);
+
+#if 0
 	XMFLOAT3 up, lookAt;
 	XMVECTOR upVector, positionVector, lookVector;
 	float yaw, pitch, roll;
-	XMMATRIX rotationMatrix;
+	XMMATRIX rotationMatrix, cica;
 
 	upVector = XMLoadFloat3(&m_up);
 
 	positionVector = XMLoadFloat3(&m_position);
+	positionVector = XMVectorSetW(positionVector, 1.f);
 
 	lookVector = XMLoadFloat3(&m_look);
+	lookVector = XMVectorSetW(lookVector, 1.f);
 
 	pitch = m_rotation.x;
-	yaw   = m_rotation.y;
-	roll  = m_rotation.z;
+	yaw = m_rotation.y;
+	roll = m_rotation.z;
 
 	rotationMatrix = XMMatrixRotationRollPitchYaw(pitch, yaw, roll);
 
-	lookVector = XMVector3TransformCoord(lookVector, rotationMatrix);
+	// lookVector = XMVector3TransformCoord(lookVector, rotationMatrix);
 	upVector = XMVector3TransformCoord(upVector, rotationMatrix);
+	upVector = XMVectorSetW(upVector, 0.f);
 
-	lookVector = XMVectorAdd(positionVector, lookVector);
-	lookVector = XMVector4Normalize(lookVector);
-	// lookVector
+	// lookVector = XMVectorAdd(positionVector, lookVector);
+	// lookVector = XMVector3Normalize(lookVector);
+	// lookVector = XMVectorSetW(lookVector, 1.f);
+
+// http://stackoverflow.com/questions/19484601/3d-first-person-camera-strafing-at-angle
+
+	
+	//m_viewMatrix = XMMatrixLookToLH(positionVector, lookVector, upVector);
+	m_viewMatrix = cica = XMMatrixLookAtLH(positionVector, lookVector, upVector);
+
+#endif // 0
 
 	renderer.GetScreenSizef(m_screenWidth, m_screenHeight);
 	this->m_aspect = m_screenWidth / m_screenHeight;
-
-	m_viewMatrix = XMMatrixLookToLH(positionVector, lookVector, upVector);
-	//m_viewMatrix = XMMatrixLookAtLH(positionVector, lookVector, upVector);
 
 	// --- projection & ortho --- 
 	m_projectionMatrix = XMMatrixPerspectiveFovLH(m_fov, m_aspect, m_znear, m_zfar);
