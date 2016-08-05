da1e7da61428ed067f2e081424a0e2a0219071d9
caiwan <caiwan.lupus@gmail.com>
2016-08-05 15:49:52 +0200

    flip and mormalize camera axes, works finally

--- src/assimpConv/main.cpp
+++ src/assimpConv/main.cpp
@@ -37,6 +37,14 @@ aiTextureType textureTypes[] = {
 	aiTextureType_REFLECTION
 };
 
+inline aiVector3D crossProduct(aiVector3D a, aiVector3D b) {
+	return aiVector3D(
+		(a.y*b.z - a.z*b.y),
+		(a.z*b.x - a.x*b.z),
+		(a.x*b.y - a.y*b.x)
+	);
+}
+
 inline void swap_vertices(aiVector3D *vertices, char order[], char polarity[]) {
 	aiVector3D v;
 	v.x = (*vertices)[order[0]] * polarity[0];
@@ -176,73 +184,32 @@ int run(int argc, char* argv[])
 	}
 	// reorder matrices
 
-	if (args.get("flip").isFound() && scene->mRootNode && scene->mRootNode->mNumChildren){
+
+	if (args.get("flip").isFound() && scene->mRootNode && scene->mRootNode->mNumChildren) {
 		string flip = args.get("flip").value();
 
 		if (flip.length() != 1) {
 			cout << args.getHelpMessage() << endl;
 			return 1;
 		}
-
+#if 1
 		// http://www.mathplanet.com/education/geometry/transformations/transformation-using-matrices
 		// http://web.iitd.ac.in/~hegde/cad/lecture/L6_3dtrans.pdf
 
-		aiMatrix4x4 axis_rotation; 
-		aiMatrix4x4 plane_reflect;
-		
+		/*aiMatrix4x4 axis_rotation;
+		aiMatrix4x4 plane_reflect;*/
+
 		/* Elforgatja egy tengely menten, es tukrozi a meroleges sikon. */
+		// fuck it
 		switch (flip.at(0)) {
-		// x, yz
-		case 'x': case 'X': 
-			axis_rotation = aiMatrix4x4(
-				1, 0, 0, 0,
-				0, -1, 0, 0,
-				0, 0, -1, 0,
-				0, 0, 0, 1
-			);
-
-			plane_reflect = aiMatrix4x4(
-				-1, 0, 0, 0,
-				0, 1, 0, 0,
-				0, 0, 1, 0,
-				0, 0, 0, 1
-			);
-
+		case 'x': case 'X':
 			break;
-		// y, xz
-		case 'y': case 'Y': 
-			axis_rotation = aiMatrix4x4(
-				-1, 0, 0, 0,
-				0, 1, 0, 0,
-				0, 0, -1, 0,
-				0, 0, 0, 1
-			);
-
-			plane_reflect = aiMatrix4x4(
-				1, 0, 0, 0,
-				0, -1, 0, 0,
-				0, 0, 1, 0,
-				0, 0, 0, 1
-			);
+		case 'y': case 'Y':
 			break;
-
-		// z, xy
-		case 'z': case 'Z': 
-			axis_rotation = aiMatrix4x4(
-				-1, 0, 0, 0,
-				0, -1, 0, 0,
-				0, 0, 1, 0,
-				0, 0, 0, 1
-			);
-
-			plane_reflect = aiMatrix4x4(
-				1, 0, 0, 0,
-				0, 1, 0, 0,
-				0, 0, -1, 0,
-				0, 0, 0, 1
-			);
-
+		case 'z': case 'Z':
 			break;
+		default:
+			cout << args.getHelpMessage() << endl;
 		}
 
 		map<string, aiMatrix4x4> matrixMap;
@@ -283,90 +250,118 @@ int run(int argc, char* argv[])
 				if (itMatrix != matrixMap.end() && itNode != nodeMap.end()) {
 
 					aiMatrix4x4 matrix = itNode->second->mTransformation;
-					itNode->second->mTransformation = /*plane_reflect * */ axis_rotation * matrix;
+
+					// sad man's low-end compensation
+					aiVector3D x(matrix.a1, matrix.b1, matrix.c1);	// x axis
+					aiVector3D U(matrix.a2, matrix.b2, matrix.c2);	// y axis = up vector
+					aiVector3D L(matrix.a3, matrix.b3, matrix.c3);	// z axis = lookat vector
+					aiVector3D E(matrix.a4, matrix.b4, matrix.c4);	// origin
+
+					//U.Set(0, 1, 0);
+					
+
+					// flip vectors
+					E.x = -E.x;
+					E.y = -E.y;
+					E.z = E.z;
+					
+					L = -E;
+					L.Normalize();
+
+					U = -U;
+
+					// screen = x,y, forward = z
+					aiVector3D s, u, f, p;
+					f = L;
+					s = crossProduct(U, f);
+					u = crossProduct(f, s);
+					p = E;
+
+					matrix = aiMatrix4x4(
+						s.x, u.x, f.x, p.x,
+						s.y, u.y, f.y, p.y,
+						s.z, u.z, f.z, p.z,
+						0, 0, 0, 1
+					);
+
+					itNode->second->mTransformation = matrix;
 
 				}
 
 			}
 		}
+#endif
 	}
 
 #if 0
-		// reorder animations
-		if (scene->HasAnimations()) {
-			for (uint i = 0; i < scene->mNumAnimations; i++) {
-				aiAnimation* anim = scene->mAnimations[i];
-				// ... reverse engineer
-				cout << anim->mName.C_Str() << endl;
-			}
+	// reorder animations
+	if (scene->HasAnimations()) {
+		for (uint i = 0; i < scene->mNumAnimations; i++) {
+			aiAnimation* anim = scene->mAnimations[i];
+			// ... reverse engineer
+			cout << anim->mName.C_Str() << endl;
 		}
+	}
 #endif //0
 
-		//// shange the up vector
-		//if (args.get("up").isFound() && scene->HasCameras()) {
-		//	// string cameraUp = args.get("up").value();
-
-		//}
-
-
-		// strip texture filenames
-		if (args.get("textures").isFound() && scene->HasMaterials()) {
-			for (uint i = 0; i < scene->mNumMaterials; i++) {
-				aiMaterial const * material = scene->mMaterials[i];
-				for (uint j = 0; j < sizeof(textureTypes) / sizeof(textureTypes[0]); j++) {
-					aiTextureType tt = textureTypes[j];
-					for (uint k = 0; k < material->GetTextureCount(tt); k++) {
-						aiString aiPath;
-						material->GetTexture(tt, k, &aiPath);
-						string path = aiPath.C_Str();
-						{
-							string s = path;
-							string::size_type n, m;
-							n = s.find_last_of('/');
-							m = s.find_last_of('\\');
-							if (n != string::npos) {
-								s = s.substr(n + 1);
-							}
-							else if (m != string::npos) {
-								s = s.substr(m + 1);
-							}
-
-							if (!s.empty()) {
-								path = s;
-							}
+	// strip texture filenames
+	if (args.get("textures").isFound() && scene->HasMaterials()) {
+		for (uint i = 0; i < scene->mNumMaterials; i++) {
+			aiMaterial const * material = scene->mMaterials[i];
+			for (uint j = 0; j < sizeof(textureTypes) / sizeof(textureTypes[0]); j++) {
+				aiTextureType tt = textureTypes[j];
+				for (uint k = 0; k < material->GetTextureCount(tt); k++) {
+					aiString aiPath;
+					material->GetTexture(tt, k, &aiPath);
+					string path = aiPath.C_Str();
+					{
+						string s = path;
+						string::size_type n, m;
+						n = s.find_last_of('/');
+						m = s.find_last_of('\\');
+						if (n != string::npos) {
+							s = s.substr(n + 1);
+						}
+						else if (m != string::npos) {
+							s = s.substr(m + 1);
 						}
 
-						// textura filenev vissza
-						const aiMaterialProperty* pp = nullptr;
-						if (aiGetMaterialProperty(material, AI_MATKEY_TEXTURE(tt, k), &pp) == AI_SUCCESS) {
-							aiMaterialProperty* pProp = const_cast<aiMaterialProperty*>(pp);
-							if (aiPTI_String == pProp->mType) {
-								size_t newLen = path.length() + 5;
-								char* newData = (char*)malloc(newLen);
-								(*(uint*)(&newData[0])) = path.length();
-								memcpy_s(&newData[4], path.length() + 1, path.c_str(), path.length() + 1);
-								free(pProp->mData);
-								pProp->mData = newData;
-								pProp->mDataLength = newLen;
-							}
+						if (!s.empty()) {
+							path = s;
+						}
+					}
+
+					// textura filenev vissza
+					const aiMaterialProperty* pp = nullptr;
+					if (aiGetMaterialProperty(material, AI_MATKEY_TEXTURE(tt, k), &pp) == AI_SUCCESS) {
+						aiMaterialProperty* pProp = const_cast<aiMaterialProperty*>(pp);
+						if (aiPTI_String == pProp->mType) {
+							size_t newLen = path.length() + 5;
+							char* newData = (char*)malloc(newLen);
+							(*(uint*)(&newData[0])) = path.length();
+							memcpy_s(&newData[4], path.length() + 1, path.c_str(), path.length() + 1);
+							free(pProp->mData);
+							pProp->mData = newData;
+							pProp->mDataLength = newLen;
 						}
 					}
 				}
 			}
 		}
-
-		// save 
-
-		if (aiExporter.Export(scene, outExtension, outFileName) != aiReturn_SUCCESS) {
-			cout << "Could not save model file" << endl;
-			cout << aiExporter.GetErrorString() << endl;
-			return 1;
-		}
-
-		return 0;
 	}
 
-	int main(int argc, char* argv[]) {
-		int result = run(argc, argv);
-		return result;
+	// save 
+
+	if (aiExporter.Export(scene, outExtension, outFileName) != aiReturn_SUCCESS) {
+		cout << "Could not save model file" << endl;
+		cout << aiExporter.GetErrorString() << endl;
+		return 1;
 	}
+
+	return 0;
+}
+
+int main(int argc, char* argv[]) {
+	int result = run(argc, argv);
+	return result;
+}
