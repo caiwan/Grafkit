1683828410f5c5f8c687209048d8e4915b749e55
caiwan <caiwan.lupus@gmail.com>
2016-08-12 11:03:31 +0200

    Quaternion wrapper + placeholders added

---
+++ src/api/math/quaternion.h
@@ -0,0 +1,72 @@
+#pragma once 
+
+#include "../render/dxtypes.h"
+#include "matrix.h"
+
+namespace Grafkit {
+	class Quaternion {
+	private:
+		dxvector q;
+	public:
+		// ctor + copy ctor 
+		Quaternion(){
+			DirectX::XMVectorSet(0, 0, 0, 0);
+		}
+
+		Quaternion(float x, float y, float z, float w) {
+			DirectX::XMVectorSet(x, y, z, w);
+		}
+
+		Quaternion(const dxvector &in) : q(in) {
+		}
+
+		Quaternion(const Quaternion &in) : q(in.q){
+		}
+
+		Quaternion(const float4 &in) {
+			q = DirectX::XMLoadFloat4(&in);
+		}
+
+		// --- setterek
+		void Set(float x, float y, float z, float w) {
+			DirectX::XMVectorSet(x, y, z, w);
+		}
+
+		// --- muveletek
+		void Slerp(const Quaternion &q0, const Quaternion &q1, float t) {
+			q = DirectX::XMQuaternionSlerp(q0.q, q1.q, t);
+		}
+
+		// --- konverziok
+		operator Grafkit::Matrix () const {
+			// a defult konstruktor miatt automatikusan fogja letrehozni
+			return DirectX::XMMatrixRotationQuaternion(q);
+		}
+
+		operator matrix () const {
+			return DirectX::XMMatrixRotationQuaternion(q);
+		}
+
+		operator float4 () const {
+			float4 res;
+			DirectX::XMStoreFloat4(&res, q);
+			return res;
+		}
+
+		// --- operator = 
+		Quaternion& operator=(const Quaternion& in) {
+			q = in.q;
+			return *this;
+		}
+
+		Quaternion& operator=(const dxvector& in) {
+			q = in;
+			return *this;
+		}
+
+		Quaternion& operator=(const float4& in) {
+			q = DirectX::XMLoadFloat4(&in);
+			return *this;
+		}
+	};
+}
---
+++ src/api/math/vector.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace Grafkit{
+	class Vectror{
+		/// ... tbd
+	};
+}
