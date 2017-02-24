47b8eedb52beff7d91af66b4275ac7d3dc28f446
caiwan <caiwan.lupus@gmail.com>
2017-02-24 17:51:55 +0100

    copy file with cmake

--- cmake/batch_configure_copy_file.cmake
+++ cmake/batch_configure_copy_file.cmake
@@ -1,5 +1,7 @@
 # ------------------------------------------------------------------------------------------
 
+# http://stackoverflow.com/questions/13470499/cmake-add-custom-command-with-dependencies-from-a-different-directory
+
 # Adds the files to the corresponding group bsed on their type and path, visual studio style
 function(batch_configure_copy_file)
 	foreach(_source IN ITEMS ${ARGN})
@@ -15,7 +17,13 @@ function(batch_configure_copy_file)
 		set(dst_path "${CMAKE_CURRENT_BINARY_DIR}/${_source_path}")
 		
 		file(MAKE_DIRECTORY ${dst_path})
-		file(COPY "${_source_rel}" DESTINATION ${dst_path})
+		
+		# file(COPY "${_source_rel}" DESTINATION ${dst_path})
+		add_custom_command(
+			OUTPUT "${_source_rel}"
+			COMMAND ${CMAKE_COMMAND} -E copy ${_source_rel} ${dst_path}
+			DEPENDS ${_source_rel}
+			)
 
 	endforeach()
 endfunction(batch_configure_copy_file)
