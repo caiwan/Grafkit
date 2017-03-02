683231973103398bb88881769dd3c4a295b6598d
caiwan <caiwan.lupus@gmail.com>
2017-03-02 16:43:08 +0100

    WIP Assimpconverter

--- src/assimpConv/main.cpp
+++ src/assimpConv/main.cpp
@@ -45,45 +45,44 @@ using namespace ideup;
 
 using namespace Grafkit;
 
-//typedef unsigned int uint;
+typedef unsigned int uint;
 
-//aiTextureType textureTypes[] = {
-//	aiTextureType_DIFFUSE,
-//	aiTextureType_SPECULAR,
-//	aiTextureType_AMBIENT,
-//	aiTextureType_EMISSIVE,
-//	aiTextureType_HEIGHT,
-//	aiTextureType_NORMALS,
-//	aiTextureType_SHININESS,
-//	aiTextureType_OPACITY,
-//	aiTextureType_DISPLACEMENT,
-//	aiTextureType_LIGHTMAP,
-//	aiTextureType_REFLECTION
-//};
-//
-//inline aiVector3D crossProduct(aiVector3D a, aiVector3D b) {
-//	return aiVector3D(
-//		(a.y*b.z - a.z*b.y),
-//		(a.z*b.x - a.x*b.z),
-//		(a.x*b.y - a.y*b.x)
-//	);
-//}
-//
-//inline void swap_vertices(aiVector3D *vertices, char order[], char polarity[]) {
-//	aiVector3D v;
-//	v.x = (*vertices)[order[0]] * polarity[0];
-//	v.y = (*vertices)[order[1]] * polarity[1];
-//	v.z = (*vertices)[order[2]] * polarity[2];
-//	*vertices = v;
-//}
+aiTextureType textureTypes[] = {
+	aiTextureType_DIFFUSE,
+	aiTextureType_SPECULAR,
+	aiTextureType_AMBIENT,
+	aiTextureType_EMISSIVE,
+	aiTextureType_HEIGHT,
+	aiTextureType_NORMALS,
+	aiTextureType_SHININESS,
+	aiTextureType_OPACITY,
+	aiTextureType_DISPLACEMENT,
+	aiTextureType_LIGHTMAP,
+	aiTextureType_REFLECTION
+};
+
+inline aiVector3D crossProduct(aiVector3D a, aiVector3D b) {
+	return aiVector3D(
+		(a.y*b.z - a.z*b.y),
+		(a.z*b.x - a.x*b.z),
+		(a.x*b.y - a.y*b.x)
+	);
+}
+
+inline void swap_vertices(aiVector3D *vertices, char order[], char polarity[]) {
+	aiVector3D v;
+	v.x = (*vertices)[order[0]] * polarity[0];
+	v.y = (*vertices)[order[1]] * polarity[1];
+	v.z = (*vertices)[order[2]] * polarity[2];
+	*vertices = v;
+}
 
 /* ================================================================================================ */
 
 class Application{
 private:
 	Arguments args;
-	AssimpLoader * loader;
-	
+
 public:
 	Application() {
 		args.add("help", 'h').description("Shows this help message.").flag(true);
@@ -94,12 +93,9 @@ public:
 		args.add("flip", 'p').description("Flips the camera 180 deg around one given axis.");
 		args.add("lh").description("convert to left handed").flag(true);
 		args.add("textures", 't').description("Strip path from texture filenames").flag(true);
-
-		loader = new Grafkit::AssimpLoader();
 	}
 
 	~Application() {
-		delete loader;
 	}
 
 public:
@@ -111,6 +107,30 @@ public:
 			return 1;
 		}
 
+		AssimpLoader * loader;
+		void *data = nullptr;
+		size_t len = 0;
+
+		FILE* fp = nullptr;
+		if (!fopen_s(&fp, args.get("i").value().c_str(), "rw")) {
+			fseek(fp, 0, SEEK_END);
+			len = ftell(fp);
+			fseek(fp, 0, SEEK_SET);
+			data = malloc(len);
+			fread_s(data, len, 1, len, fp);
+			fclose(fp);
+		}
+		else {
+			cout << ("PENIS PENIS PENIS");
+			return 1;
+		}
+
+		loader = new Grafkit::AssimpLoader(data, len);
+
+		loader->Load();
+
+		free(data);
+
 		return 0;
 	}
 
@@ -118,19 +138,7 @@ public:
 
 /* ================================================================================================ */
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow)
-{
+int main(int argc, char* argv[]) {
 	Application app;
-
-	int argc = 0;
-
-	// http://alter.org.ua/docs/win/args/
-	char** argv = CommandLineToArgvA(GetCommandLine(), &argc);
-
 	return app.execute(argc, argv);
 }
-
-//int main(int argc, char* argv[]) {
-//	Application app;
-//	return app.run(argc, argv);
-//}
