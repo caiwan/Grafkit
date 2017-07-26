bdb84c53e110dcecaa8a34a9d02fda9b2e9ea760
caiwan <caiwan.lupus@gmail.com>
2017-07-26 23:40:42 +0200

    update assests on tests

--- tests/assets/conv.bat
+++ tests/assets/conv.bat
@@ -5,10 +5,11 @@ rem caiwan/IR
 
 set basedir=%CD%
 
-cd ..\..\msvc\deploy\tools\
+cd ..\..\msvc17\deploy\tools\
 
 AssimpConverter.exe -b -lh -i %basedir%\models\locRotScale.blend -o %basedir%\locRotScale.scene
 AssimpConverter.exe -b -lh -i %basedir%\models\sphere.blend -o %basedir%\sphere.scene
+AssimpConverter.exe -b -lh -i %basedir%\models\spheres.blend -o %basedir%\spheres.scene
 rem AssimpConverter.exe -b -lh -i %basedir%\models\scene3.blend -o %basedir%\scene3.scene
 
 cd %basedir%
