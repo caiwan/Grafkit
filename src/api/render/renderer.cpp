d34b43d2f2e7dd960c516b1a502a7c720e223d6d
caiwan <caiwan.lupus@gmail.com>
2017-08-10 16:10:09 +0200

    revert present

--- src/api/render/renderer.cpp
+++ src/api/render/renderer.cpp
@@ -431,12 +431,12 @@ void Renderer::EndScene()
 	if (m_vsync_enabled)
 	{
 		// Lock to screen refresh rate.
-		m_swapChain->Present1(1, 0);
+		m_swapChain->Present(1, 0);
 	}
 	else
 	{
 		// Present as fast as possible.
-		m_swapChain->Present1(0, 0);
+		m_swapChain->Present(0, 0);
 	}
 
 	//this->m_device->pre
