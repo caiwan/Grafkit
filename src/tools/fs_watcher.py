5091159cc0266a3f6d85bfae9f1df60c221138c7
caiwan <caiwan.lupus@gmail.com>
2017-08-24 15:45:14 +0200

    fs watcher done

--- src/tools/fs_watcher.py
+++ src/tools/fs_watcher.py
@@ -1,18 +1,61 @@
 import sys
 import time
 import logging
+import os
+import shutil
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
+from watchdog.events import LoggingEventHandler, FileSystemEventHandler
 
+def print_usage():
+    print('QnD File system watcher')
+    print('- copies modified file to another dir')
+    print('')
+    print('usage: fs_watcher.py <input_dir> <output_file>')
+
+
+class EvtHandler(FileSystemEventHandler): 
+    def __init__(self, dst_dir):
+        FileSystemEventHandler.__init__(self)
+        self.dst = dst_dir
+        self.lg = logging.getLogger('EvtHandler')
+        pass
+        
+    def on_created(self, evt):
+        try:
+            shutil.copy(evt.src_path, self.dst)
+            self.lg.info("Copy file " + evt.src_path)
+        except:
+            pass
+        pass
+        
+    def on_modified(self, evt):
+        try :
+            shutil.copy(evt.src_path, self.dst)
+            self.lg.info("Copy file " + evt.src_path)
+        except:
+            pass
+        pass        
+    pass
+    
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    path = sys.argv[1] if len(sys.argv) > 1 else './'
-    event_handler = LoggingEventHandler()
+
+    if len(sys.argv) != 3:
+        print_usage()
+        sys.exit(0)
+
+    input = sys.argv[1]
+    output = sys.argv[2]
+        
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+                        
+    event_handler_log = LoggingEventHandler()
+    event_handler = EvtHandler(output)
+    
     observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
+    observer.schedule(event_handler_log, input, recursive=True)
+    observer.schedule(event_handler, input, recursive=True)
     observer.start()
+    
     try:
         while True:
             time.sleep(1)
---
+++ watchAssets.bat
@@ -0,0 +1,4 @@
+@echo off
+
+rem qnd way to watch and autocopy assets
+python src/tools/fs_watcher.py ./tests/assets/shaders/ ./msvc/tests/assets/shaders/
