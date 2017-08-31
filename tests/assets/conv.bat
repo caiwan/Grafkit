4553115c57e0cbbb7f88557e261d79a1b05192d5
caiwan <caiwan.lupus@gmail.com>
2017-08-31 10:51:26 +0200

    Camera movement test added

--- tests/assets/conv.bat
+++ tests/assets/conv.bat
@@ -10,6 +10,7 @@ cd ..\..\msvc\deploy\tools\
 AssimpConverter.exe -b --lh -i %basedir%\models\animtest1.blend -o %basedir%\animtest1.scene
 AssimpConverter.exe -b --lh -i %basedir%\models\animtest2.blend -o %basedir%\animtest2.scene
 
+AssimpConverter.exe -b --lh -i %basedir%\models\locRotCamera.blend -o %basedir%\locRotCamera.scene
 AssimpConverter.exe -b --lh -i %basedir%\models\locRotScale.blend -o %basedir%\locRotScale.scene
 AssimpConverter.exe -b --lh -i %basedir%\models\sphere.blend -o %basedir%\sphere.scene
 AssimpConverter.exe -b --lh -i %basedir%\models\spheres.blend -o %basedir%\spheres.scene
