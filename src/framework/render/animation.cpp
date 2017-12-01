420a7539215bdd7146bbe0849a6ae4954d711ba6
caiwan <caiwan.lupus@gmail.com>
2017-12-01 17:07:48 +0100

    fix this crap

--- src/framework/render/animation.cpp
+++ src/framework/render/animation.cpp
@@ -32,26 +32,26 @@ void Grafkit::Animation::FindKey(FloatTrack track, double &time, float &value)
 {
 	float t = (float)time, f = 0.f;
 	float v0 = 0., v1 = 0.;
-	if (track.FindKey(t, v0, v1, f))
-		value = LERP(v0, v1, f);
+	track.FindKey(t, v0, v1, f);
+	value = LERP(v0, v1, f);
 }
 
 void Grafkit::Animation::FindKey(Vector3Track track, double &time, float3 &value)
 {
 	float t = (float)time, f = 0.f;
 	float3 v0(0, 0, 0), v1(0, 0, 0);
-	if (track.FindKey(t, v0, v1, f))
-		value = LERPv3(v0, v1, f);
+	track.FindKey(t, v0, v1, f);
+	value = LERPv3(v0, v1, f);
 }
 
 void Grafkit::Animation::FindKey(Vector4Track track, double &time, Quaternion &value)
 {
 	float t = (float)time, f = 0.f;
 	float4 v0, v1;
-	if (track.FindKey(t, v0, v1, f)) {
-		Quaternion q0(v0), q1(v1);
-		value.Slerp(q0, q1, f);
-	}
+	track.FindKey(t, v0, v1, f);
+	Quaternion q0(v0), q1(v1);
+	value.Slerp(q0, q1, f);
+
 }
 
 /* ============================================================================================== */
@@ -61,7 +61,7 @@ void Grafkit::ActorAnimation::Update(double time)
 	float3 pos(0, 0, 0), scale(1, 1, 1);
 	Quaternion rotate;
 	Matrix matrix;
-	
+
 	Animation::FindKey(m_positionTrack, time, pos);
 	Animation::FindKey(m_scalingTrack, time, scale);
 	Animation::FindKey(m_rotationTrack, time, rotate);
@@ -73,7 +73,7 @@ void Grafkit::ActorAnimation::Update(double time)
 
 	// note:
 	// https://gamedev.stackexchange.com/questions/16719/what-is-the-correct-order-to-multiply-scale-rotation-and-translation-matrices-f
-	
+
 	m_actor->Matrix(matrix);
 }
 
@@ -99,7 +99,7 @@ void Grafkit::ActorAnimation::serialize(Archive & ar)
 	this->Animation::_serialize(ar);
 
 	m_positionTrack.serialize(ar);
-	m_scalingTrack .serialize(ar);
+	m_scalingTrack.serialize(ar);
 	m_rotationTrack.serialize(ar);
 }
 
@@ -109,8 +109,8 @@ void Grafkit::CameraAnimation::Update(double t)
 {
 	Camera* cam = dynamic_cast<Camera*>(m_entity.Get());
 	if (cam) {
-		float fov = 0; 
-		FindKey(m_fov, t, fov); 
+		float fov = 0;
+		FindKey(m_fov, t, fov);
 		cam->SetFOV(fov);
 		// focal shit??
 	}
