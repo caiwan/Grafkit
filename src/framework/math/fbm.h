42e663a92031a0377e400e936ba3b0de2cdede9f
caiwan <caiwan.lupus@gmail.com>
2017-12-13 16:10:13 +0100

    Rnd

--- src/framework/math/fbm.h
+++ src/framework/math/fbm.h
@@ -1,53 +1,41 @@
 
-// obsolete.
-
 #pragma once 
 
 #include <cmath>
 
-#include "common.h"
+#include "../render/dxtypes.h"
 
 /*
 QND reverse-engineered Fractional Brownian motion
 Avoid using it, and please rely on shaders
 */
 
+// tu make sure it will genrate the same on each run
+#define SEED 0xe4d07f97 
+
 namespace Grafkit {
+	class PseudoRandom {
+	public:
+		static inline float Random() {
+			static int inited = 0;
+			if (!inited) {
+				srand(SEED);
+				inited = 1;
+			}
+			return (float)rand() / (float)RAND_MAX;
+		}
 
-	float fractf(float f) {
-		return f - (int)f;
-	}
+		static inline float2 Random2D() {
+			return float2(Random(), Random());
+		}
 
-	float dot(float2 v, float2 w) {
-		return v.x * w.x + v.y * w.y;
-	}
+		static inline float3 Random3D() {
+			return float3(Random(), Random(), Random());
+		}
+	};
 
-//float hash2(float2 co){
-//static float[64*64] lut;
-//static int fisttime = 0;
-//
-//}
+	// Fractional brownian motion
+	class FBM {
 
-	float hash(float2 co) {
-		float a = 12.9898, b = 78.233, c = 43758.5453;
-		float dt = dot(co, float2(a, b));
-		float sn = fmod(dt, 3.14);
-		return fractf(sin(sn) * c);
-	}
-
-	float2 _m2(float2 p) {
-		float2 r;
-		r.x = p.x * 1.6 + p.y * -1.2;
-		r.x = p.x * 1.2 + p.y * 1.6;
-		return r;
-	}
-
-	float fbm(float2 p)
-	{
-		float f = 0.5000*hash(p); p = _m2(p);
-		f += 0.2500*hash(p); p = _m2(p);
-		f += 0.1666*hash(p); p = _m2(p);
-		f += 0.0834*hash(p);
-		return f;
-	}
+	};
 }
