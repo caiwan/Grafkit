819dc8c82398c15e6f4ca940deac63b27b1099fe
caiwan <caiwan.lupus@gmail.com>
2017-12-17 13:13:50 +0100

    Fix cxmake for dev and prod build + assets as file w/o  manual overriding

--- create15_2017.bat
+++ create15_2017.bat
@@ -9,4 +9,4 @@ cd msvc
 cmake -DCMAKE_INSTALL_PREFIX=deploy/dev -DBUILD_LIVE_RELEASE=FALSE -DCMAKE_PREFIX_PATH=%CMAKE_PREFIX_PATH% -G "Visual Studio 15 2017 Win64" ../ 
 cd ..
 
-python src\tools\sln_disable_shader_compil.py msvc/
+rem python src\tools\sln_disable_shader_compil.py msvc/
