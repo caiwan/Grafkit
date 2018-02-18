ccf920de75a6851793ea501007f41859a83d1596
caiwan <caiwan.lupus@gmail.com>
2018-02-18 14:41:58 +0100

    WIP curveeditor

--- src/editor/ui/curve/curveeditorwidget.h
+++ src/editor/ui/curve/curveeditorwidget.h
@@ -1,28 +1,6 @@
-/***************************************************************************
- *   Copyright (C) 2012                                                    *
- *   Anatole Duprat <anatole.duprat@gmail.com>                             *
- *   Charles Bulckaen  <xtrium@frequency.fr>                               *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or         *
- *   modify it under the terms of the GNU General Public License           *
- *   as published by the Free Software Foundation; either version 2        *
- *   of the License, or (at your option) any later version.                *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA          *
- ***************************************************************************/
-
 #if 0
 
-#ifndef CURVEEDITORWIDGET_HH
-#define CURVEEDITORWIDGET_HH
+#pragma once 
 
 #include <QtGui>
 
@@ -74,7 +52,5 @@ private:
     Ui::CurveEditorWidget *ui;
     CurveEditorScene* _ces;
 };
-
-#endif // CURVEEDITORWIDGET_HH
 
 #endif
