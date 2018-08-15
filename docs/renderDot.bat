68e9772a31619c61bf19ed648e3f67147fc4678d
Caiwan <caiwan.lupus@gmail.com>
2018-08-15 21:31:40 +0200

    Remove docs

--- docs/renderDot.bat
+++
@@ -1,2 +0,0 @@
-"c:\Program Files (x86)\Graphviz2.38\bin\dot.exe" -Tpng eventFlow.dot -o eventFlow.png
-"c:\Program Files (x86)\Graphviz2.38\bin\dot.exe" -Tpng dependencyInjection.dot -o dependencyInjection.png
